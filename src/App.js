771cb1bbf6071233a1a8573c1d6fcd3f91590fae
RoGal1k <80055564+roGal1k@users.noreply.github.com>
2023-06-27 14:48:58 +0600

    add ifer-method for modal window in projects

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import React from "react";
 import "./index.scss";
 
 function App() {
-  const [count, updateCount] = React.useState(0);
-
-  const onClickPlus = () => {
-    updateCount(count + 1);
-    console.log(count);
-  };
-
-  const onClickMinus = () => {
-    if (count !== 0) updateCount(count - 1);
-    console.log(count);
-  };
+  const [open, setOpen] = React.useState(false);
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button on onClick={onClickMinus} className="minus">
-          - Минус
-        </button>
-        <button onClick={onClickPlus} className="plus">
-          Плюс +
-        </button>
-      </div>
+      <button onClick={() => setOpen(true)} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+      {open && (
+        <div className="overlay">
+          <div className="modal">
+            <svg
+              onClick={() => setOpen(false)}
+              height="200"
+              viewBox="0 0 200 200"
+              width="200"
+            >
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
