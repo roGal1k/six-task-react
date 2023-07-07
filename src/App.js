7abce0ef0c2ef2c9a687f022c6f11331ffbae270
RoGal1k <80055564+roGal1k@users.noreply.github.com>
2023-07-08 00:10:49 +0600

    addphotos-gallery

--- src/App.js
+++ src/App.js
@@ -1,31 +1,76 @@
-import React from "react";
-import "./index.scss";
+import React from 'react';
+import './index.scss';
+
+import { Collection } from './Collection';
+
+const CATEGORYS = [
+  { "name": "Все" },
+  { "name": "Море" },
+  { "name": "Горы" },
+  { "name": "Архитектура" },
+  { "name": "Города" }
+];
 
 function App() {
-  const [count, updateCount] = React.useState(0);
 
-  const onClickPlus = () => {
-    updateCount(count + 1);
-    console.log(count);
-  };
+  const [collections, setCollections] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [categoryActive, setCategoryActive] = React.useState(0);
+  const [page, setPage] = React.useState(1);
 
-  const onClickMinus = () => {
-    if (count !== 0) updateCount(count - 1);
-    console.log(count);
-  };
+  const category = categoryActive ? `category=${categoryActive}`: 0;
+
+  React.useEffect(()=>{
+    setIsLoading(true);
+    fetch(`https://64a3a005c3b509573b565443.mockapi.io/api/collection_task?page=${page}&limit=3&${category}`)
+      .then(res => res.json())
+      .then(json=> {setCollections(json);})
+      .catch(err=>{console.warn(err);alert("Error connection")})
+      .finally(()=>setIsLoading(false));
+      console.log(`https://64a3a005c3b509573b565443.mockapi.io/api/collection_task?page=${page}&limit=3&category=${category}`);},
+      [categoryActive,page]);
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button on onClick={onClickMinus} className="minus">
-          - Минус
-        </button>
-        <button onClick={onClickPlus} className="plus">
-          Плюс +
-        </button>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {
+            CATEGORYS.map((obj,i) => <li
+              onClick={()=>setCategoryActive(i)}
+              className = {categoryActive=== i ? 'active' : ''} 
+              key={obj.name}>
+                 {obj.name} </li>)
+          }
+        </ul>
+        <input 
+          value = {searchValue}
+          onChange={e=>setSearchValue(e.target.value)}
+          className="search-input" 
+          placeholder="Поиск по названию" />
       </div>
+      <div className="content">
+        {isLoading ? <h2>Loading...</h2> :
+        collections.filter(obj => obj.name.toLowerCase().includes(searchValue.toLowerCase())).map((obj,i) => (
+          <Collection
+            key = {i}
+            name={obj.name}
+            images={obj.photos}
+          />
+        ))
+        }
+      </div>
+      <ul className="pagination">
+        {
+        [...Array(3)].map((_, i) =>(
+          <li
+          key={i}
+          onClick={() => setPage(i+1)}
+          className={page ===i+1? 'active' : ''}
+          >{i+1}</li>
+        ))}
+      </ul>
     </div>
   );
 }
